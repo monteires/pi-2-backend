0de59701606aca9ba903e413f37ad7d14880d441
Paulo <2008238@aluno.univesp.br>
2022-06-28 23:26:40 -0300

    update caminho imagem

--- controller/photo.controller.js
+++ controller/photo.controller.js
@@ -12,7 +12,7 @@ const multer = require('multer');
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         // cb(null, 'public/uploads/');
-        cb(null, path.join(__dirname, '/../public/uploads'));
+        cb(null, path.join(__dirname, 'public/uploads'));
 
     },
     filename: (req, file, cb) => {
